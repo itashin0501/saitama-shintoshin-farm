b8dbedf8e206bec8342902bae165786b79393201
itamoto shintaro <itashin0501@gmail.com>
2025-06-29 17:35:58 +0900

    文言修正

--- src/app/(authenticated)/events/[event_date]/page.tsx
+++ src/app/(authenticated)/events/[event_date]/page.tsx
@@ -53,14 +53,13 @@ export default function EventDatePage() {
         <h2 className="text-2xl font-bold text-green-800 mb-2 text-center">
           フルーツとやさい畑の皆様へ
         </h2>
-        <pre className="text-gray-700 mb-4 text-center whitespace-pre-line">
-          既に暑さも増してきておりますが、皆様いかがお過ごしでしょうか。
-          農園ご利用の皆様とそのご家族・ご友人を対象に、農園のピザ窯を使ったイベントを開催いたします。
-        </pre>
-        <pre className="text-gray-700 whitespace-pre-line">
-          農園で収穫した新鮮な野菜をピザに乗せてみたり、みんなでランチを楽しみませんか？
-          焼きたてのピザを味わいながら、交流や親睦を深める機会となれば幸いです。
-          ご家族やお友達もお誘い合わせのうえ、ぜひご参加ください！
+        <pre className="text-gray-700 text-xl my-4 text-center">
+          {`既に暑さも増してきておりますが、皆様いかがお過ごしでしょうか。
+農園ご利用の皆様とそのご家族・ご友人を対象に、農園のピザ窯を使ったイベントを開催いたします。
+
+農園で収穫した新鮮な野菜をピザに乗せてみたり、みんなでランチを楽しみませんか？
+焼きたてのピザを味わいながら、交流や親睦を深める機会となれば幸いです。
+ご家族やお友達もお誘い合わせのうえ、ぜひご参加ください！`}
         </pre>
       </div>
       <div className="bg-white rounded-lg shadow-xl p-8 mb-8 border-t-4 border-orange-400">
@@ -86,7 +85,7 @@ export default function EventDatePage() {
                 "https://firebasestorage.googleapis.com/v0/b/fruits-hatake.firebasestorage.app/o/farmEntrance.jpg?alt=media&token=73bf8535-4fde-4ba1-8ca2-4b890fcf4348",
                 "https://firebasestorage.googleapis.com/v0/b/fruits-hatake.firebasestorage.app/o/12129.jpg?alt=media&token=be894823-8dbb-477f-be1b-e2a75a0ca537",
                 "https://firebasestorage.googleapis.com/v0/b/fruits-hatake.firebasestorage.app/o/potato.jpg?alt=media&token=c5f3a7a2-6f78-4648-960d-e9bcfc49bc94",
-                "https://firebasestorage.googleapis.com/v0/b/fruits-hatake.firebasestorage.app/o/privatepark.jpg?alt=media&token=f771e00a-6b93-46d1-b60f-d2e715338d9c",
+                "https://firebasestorage.googleapis.com/v0/b/fruits-hatake.firebasestorage.app/o/privatepark.jpg?alt=media&token=f771e00a-6b93-46d1-b60f-d2e715338d9c   ",
               ]}
             />
           </div>
@@ -235,8 +234,13 @@ export default function EventDatePage() {
 
       <div className="bg-green-600 text-white rounded-lg shadow-lg p-8 text-center">
         <h3 className="text-2xl font-bold mb-4 flex items-center justify-center">
-          <PhoneIcon className="mr-2" />
-          参加ご希望の方
+          <div className="flex flex-col items-center">
+            <p>
+              <PhoneIcon className="mr-2" />
+              参加ご希望の方へ
+            </p>
+            <p>食材等の用意のため、前日までにご連絡お願い致します。</p>
+          </div>
         </h3>
         <div className="bg-white text-green-600 inline-block px-6 py-3 rounded-lg font-semibold">
           フルーツとやさい畑 : LINEにて参加人数をお知らせください。
